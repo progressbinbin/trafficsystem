6e24a7d9106f4ceff83a177cdfb9fad02d548ee9
binbinyouli <binbinyouli@qq.com>
2018-12-10 16:40:10 +0800

    添加entity 实体类，全局应用于分页、返回参数、状态码

--- traffic_common/src/main/java/entity/Result.java
+++ traffic_common/src/main/java/entity/Result.java
@@ -4,4 +4,48 @@ package entity;
  * Created by yuantousanfen on 2018/12/10.
  */
 public class Result {
+    private boolean flag;
+    private Integer code;
+    private String message;
+    private Object data;
+    public Result(){
+
+    }
+
+    public Result(boolean flag, Integer code, String message) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result(boolean flag, Integer code, String message, Object data) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
